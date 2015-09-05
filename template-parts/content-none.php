3cccea4794dd4f3aace83350af89362e8e3215a0
Jon Rivers <jerivers3@gmail.com>
2015-09-05 18:24:43 -0400

    Commit

---
+++ template-parts/content-none.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @link https://codex.materialize.org/Template_Hierarchy
+ *
+ * @package gridmat
+ */
+
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'gridmat' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'gridmat' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+
+		<?php elseif ( is_search() ) : ?>
+
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'gridmat' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php else : ?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'gridmat' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
